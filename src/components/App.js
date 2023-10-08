3987c0d6971de3d20bfeaec012d370cf1ff6f3d2
pratik2820 <pratikmorge28@gmail.com>
2023-10-08 11:10:59 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+
+
+  const[weatherData,setWeatherData] = useState({temperature:0,conditions:""})
+
+  useEffect(()=>{
+    setWeatherData({temperature:25,conditions:"Sunny"})
+  },[])
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <WeatherDisplay weatherData={weatherData}/>
+        
     </div>
   )
 }
